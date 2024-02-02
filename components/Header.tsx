1d6ed7c86f2dd5ceff226cf6ac5db64275f3f6d8
Flaviota <flaviota.dev@gmail.com>
2024-02-02 17:18:57 -0300

    fix: add in headers

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,23 +11,30 @@ import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "@/hooks/useUsex";
+import { FaUserAlt } from "react-icons/fa";
 
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({
-     children, 
-     className
-     }) => {
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
   const authModal = useAuthModal();
   const router = useRouter();
 
   const supabaseClient = useSupabaseClient();
-  const {} = useUser();
+  const { user } = useUser();
 
-  const handleLogout = () => {};
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+
+    //TODO:Reset any playing songs
+    router.refresh();
+
+    if (error) {
+      console.log(error);
+    }
+  };
   return (
     <div
       className={twMerge(
@@ -126,32 +133,46 @@ const Header: React.FC<HeaderProps> = ({
                 gap-x-4
                 "
         >
-          <>
-            <div>
+          {user ? (
+            <div className="flex gap-x items-center">
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                Logout
+              </Button>
               <Button
-                onClick={authModal.onOpen}
-                className="
+              onClick={() => router.push('/account')}
+              className="bg-white"
+              >
+                <FaUserAlt />
+              </Button>
+            </div>
+          ) : (
+            <>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="
                         bg-transparent
                         text-neutral-300
                         font-medium
                         "
-              >
-                Sign up
-              </Button>
-            </div>
-            <div>
-              <Button
-                onClick={authModal.onOpen}
-                className="
+                >
+                  Sign up
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="
                         bg-white
                         px-6
                         py-2
                         "
-              >
-                Login
-              </Button>
-            </div>
-          </>
+                >
+                  Login
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
