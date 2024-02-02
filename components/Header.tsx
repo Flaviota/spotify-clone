ab6114d97ff3d0341173a7312ffe0b3b3c8f988d
Flaviota <flaviota.dev@gmail.com>
2024-02-01 22:09:29 -0300

    feat: news useAuthModal

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,49 +8,58 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 
 import Button from "./Button";
-
+import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUsex";
 
 interface HeaderProps {
-    children: React.ReactNode;
-    className?: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
 const Header: React.FC<HeaderProps> = ({
-    children,
-    className,
-}) => {
-    const router = useRouter();
+     children, 
+     className
+     }) => {
+  const authModal = useAuthModal();
+  const router = useRouter();
 
-    const handleLogout = () => {
+  const supabaseClient = useSupabaseClient();
+  const {} = useUser();
 
-    }
-    return (
-        <div 
-        className={twMerge(`
+  const handleLogout = () => {};
+  return (
+    <div
+      className={twMerge(
+        `
         h-fit
         bg-gradient-to-b
         from-emerald-800
         p-6
         `,
         className
-        )}
-        >
-            <div className="
+      )}
+    >
+      <div
+        className="
             w-full
             mb-4
             flex
             items-center
             justify-between
-            ">
-                <div className="
+            "
+      >
+        <div
+          className="
                 hidden
                 md:flex
                 gap-x-2
                 items-center
-                ">
-                    <button
-                    onClick={() =>  router.back()}
-                    className="
+                "
+        >
+          <button
+            onClick={() => router.back()}
+            className="
                     rounded-full
                     bg-black
                     flex
@@ -59,13 +68,13 @@ const Header: React.FC<HeaderProps> = ({
                     hover:opacity-75
                     transition
                     "
-                    >
-                        <RxCaretLeft className="text-white" size={35}/>
-                    </button>
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
 
-                    <button
-                    onClick={() =>  router.forward()}
-                    className="
+          <button
+            onClick={() => router.forward()}
+            className="
                     rounded-full
                     bg-black
                     flex
@@ -74,13 +83,13 @@ const Header: React.FC<HeaderProps> = ({
                     hover:opacity-75
                     transition
                     "
-                    >
-                        <RxCaretRight className="text-white" size={35}/>
-                    </button>
-                </div>
-                <div className="flex md:hidden gap-x-2 items-center">
-                    <button
-                    className="
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button
+            className="
                     rounded-full
                     p-2
                     bg-white
@@ -90,12 +99,12 @@ const Header: React.FC<HeaderProps> = ({
                     hover:opacity-75
                     transition
                     "
-                    >
-                        <HiHome className="text-black" size={20}/>
-                    </button>
+          >
+            <HiHome className="text-black" size={20} />
+          </button>
 
-                    <button
-                    className="
+          <button
+            className="
                     rounded-full
                     p-2
                     bg-white
@@ -105,47 +114,49 @@ const Header: React.FC<HeaderProps> = ({
                     hover:opacity-75
                     transition
                     "
-                    >
-                        <BiSearch className="text-black" size={20}/>
-                    </button>
-                </div>
-                <div className="
+          >
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div
+          className="
                 flex
                 justify-between
                 items-center
                 gap-x-4
                 "
-                >
-                    <>
-                    <div>
-                        <Button
-                        className="
+        >
+          <>
+            <div>
+              <Button
+                onClick={authModal.onOpen}
+                className="
                         bg-transparent
                         text-neutral-300
                         font-medium
                         "
-                        >
-                            Sign up
-                            </Button>
-                    </div>
-                    <div>
-                        <Button
-                        onClick={() => {}}
-                        className="
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button
+                onClick={authModal.onOpen}
+                className="
                         bg-white
                         px-6
                         py-2
                         "
-                        >
-                            Login
-                            </Button>
-                    </div>
-                    </>
-                </div>
+              >
+                Login
+              </Button>
             </div>
-            {children}
+          </>
         </div>
-    );
-}
+      </div>
+      {children}
+    </div>
+  );
+};
 
 export default Header;
