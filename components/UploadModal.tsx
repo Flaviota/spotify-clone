908bda34612dbb549b4c3ee8a30009524fa53701
Flaviota <flaviota.dev@gmail.com>
2024-02-29 23:17:31 -0300

    reset in modal

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,11 +1,22 @@
+"use client"
+
+import useUploadModal from "@/hooks/useOploadModal";
 import Modal from "./Modal";
 
 const UploadModal = () => {
+    const uploadModal = useUploadModal();
+
+    const onChange = (open: boolean) => {
+        if (!open) {
+            //Reset the from
+        }
+    }
+
     return (
         <Modal
         title="Upload modal title"
         description="Upload modal description"
-        isOpen
+        isOpen={uploadModal.isOpen}
         onChange={() => {}}
         >
             Upload Content
