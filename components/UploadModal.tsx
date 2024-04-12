2d2174c57a541a0d84b24d1b1675e2f9a6122ccf
Flaviota <flaviota.dev@gmail.com>
2024-04-11 22:04:55 -0300

    feat: New implementation Input component and restored supabase data

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,27 +1,52 @@
-"use client"
+"use client";
+import { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useUploadModal from "@/hooks/useOploadModal";
 import Modal from "./Modal";
+import Input from "./Input";
 
 const UploadModal = () => {
-    const uploadModal = useUploadModal();
+  const [isLoading, setIsLoading] = useState();
+  const uploadModal = useUploadModal();
 
-    const onChange = (open: boolean) => {
-        if (!open) {
-            //Reset the from
-        }
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      reset();
+      uploadModal.onclose();
     }
+  };
 
-    return (
-        <Modal
-        title="Upload modal title"
-        description="Upload modal description"
-        isOpen={uploadModal.isOpen}
-        onChange={() => {}}
-        >
-            Upload Content
-        </Modal>
-    );
-}
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    // Upload to supabase
+  };
+
+  return (
+    <Modal
+      title="Add a song"
+      description="Upload an mp3 file"
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song title"
+        />
+      </form>
+    </Modal>
+  );
+};
 
 export default UploadModal;
